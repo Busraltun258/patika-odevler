b8991498cfd26f9ad96e9fa698c4f6286314d244
BÜŞRA ALTUN <71510210+Busraltun258@users.noreply.github.com>
2023-02-22 18:39:41 +0300

    Update CinZodyagıBurcu.java

--- patika/CinZodyagıBurcu.java
+++ patika/CinZodyagıBurcu.java
@@ -28,52 +28,45 @@ public class CinZodyagıBurcu {
                 break;
             case 2:
                 burc = "Köpek";
-                System.out.println("Çin Zodyağı Burcunuz: " + burc);
+              
                 break;
             case 3:
                 burc = "Domuz";
-                System.out.println("Çin Zodyağı Burcunuz: " + burc);
+                
                 break;
             case 4:
                 burc = "Fare";
-                System.out.println("Çin Zodyağı Burcunuz: " + burc);
+                
                 break;
             case 5:
                 burc = "Öküz";
-                System.out.println("Çin Zodyağı Burcunuz: " + burc);
                 break;
             case 6:
                 burc = "Kaplan";
-                System.out.println("Çin Zodyağı Burcunuz: " + burc);
                 break;
             case 7:
                 burc = "Tavşan";
-                System.out.println("Çin Zodyağı Burcunuz: " + burc);
                 break;
             case 8:
                 burc = "Ejderha";
-                System.out.println("Çin Zodyağı Burcunuz: " + burc);
                 break;
             case 9:
                 burc = "Yılan";
-                System.out.println("Çin Zodyağı Burcunuz: " + burc);
                 break;
             case 10:
                 burc = "At";
-                System.out.println("Çin Zodyağı Burcunuz: " + burc);
                 break;
             case 11:
                 burc = "Koyun";
-                System.out.println("Çin Zodyağı Burcunuz: " + burc);
+                
                 break;
+        }
+        System.out.println("Çin Zodyağı Burcunuz: " + burc);
 
-
-            default:
-                System.out.print("Hatalı giriş yaptınız: 0 ve 11 arası bir değer girin: ");
 
         }
 
 
     }
-}
+
 
